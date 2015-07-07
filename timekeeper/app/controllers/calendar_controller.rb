1133475a4a07d61d134f831e936f0065ed8b0da4
Katrina Eaton <katrina.eaton@gmail.com>
2015-07-07 13:32:53 -0700

    possible half month increment sol'n

--- timekeeper/app/controllers/calendar_controller.rb
+++ timekeeper/app/controllers/calendar_controller.rb
@@ -1,5 +1,15 @@
 class CalendarController < ApplicationController
   def show
-  	@date = params[:date] ? Date.parse(params[:date]) : Date.today
+  	# @date = params[:date] ? Date.parse(params[:date]) : Date.today
+  	@today = Date.today
+  	if params[:date]
+  		@date = Date.parse(params[:date])
+  	else
+  		if Date.today.day >= 16
+  			@date = Date.new(@today.year, @today.month, 16)
+  		else
+  			@date = Date.new(@today.year, @today.month, 1)
+  		end
+  	end
   end
 end
--- timekeeper/lib/calendar.rb
+++ timekeeper/lib/calendar.rb
@@ -1,29 +1,45 @@
-require 'date'
+
 
 class Calendar < Struct.new(:view, :date, :callback)
 	HEADER = %w[Date Prog1 Prog2 Prog3 Prog4 Subtotal]
-	START_DAY = :sunday
 
 	delegate :content_tag, to: :view
 
 	def table
 		content_tag :table, class: "calendar table table-bordered" do
-				header + week_rows
+				if date.strftime('%d').to_i >= 16
+					header + pay_period2
+				else
+					header + pay_period1
+				end
 		end
 	end
 
 	def header
 		content_tag :tr do
-			HEADER.map { |day| content_tag :th, day, :colspan => 2 }.join.html_safe
+			HEADER.map { |column| content_tag :th, column, :colspan => 2 }.join.html_safe
 		end
 	end
 
-	def week_rows
-		weeks.map do |week|
+	def pay_period1
+		days1.map do |day|
 			content_tag :tr do
-				week.map { |day| day_cell(day) }.join.html_safe
+				day.map { |day| day_cell(day) }.join.html_safe
+				# day.map { |day| day_name(day) }.join.html_safe
 			end
 		end.join.html_safe
+	end
+
+	def pay_period2
+		days2.map do |day|
+			content_tag :tr do
+				day.map { |day| day_cell(day)}.join.html_safe
+			end
+		end.join.html_safe
+	end
+
+	def day_name(day)
+		content_tag :td, view.capture(day.strftime('%a'), &callback), class: day_classes(day)
 	end
 
   def day_cell(day)
@@ -31,21 +47,26 @@ class Calendar < Struct.new(:view, :date, :callback)
   end
 
   def day_classes(day)
-  	# current_day = Date.parse(day)
     classes = []
     classes << "grey" if day.strftime('%a') == "Sun"
     classes << "grey" if day.strftime('%a') == "Sat"
     classes.empty? ? nil : classes.join(" ")
   end
 
-	def weeks
-		# first = date.beginning_of_month.beginning_of_week(START_DAY)
-
-		first = Date.new(date.year, date.month, 1)		
-		last = Date.new(date.year, date.month, -1)
-    # last = date.end_of_month.end_of_week(START_DAY)
-		(first..last).to_a.in_groups_of(1)
+	def days1
+		# if date.day <= 15
+			first = Date.new(date.year, date.month, 1)		
+			fifteen = Date.new(date.year, date.month, 15)
+		# end
+		(first..fifteen).to_a.in_groups_of(1)
 	end
+
+	def days2
+		sixteen = Date.new(date.year, date.month, 16)
+		last = Date.new(date.year, date.month, -1)
+		(sixteen..last).to_a.in_groups_of(1)
+	end
+
 end
 			
 
